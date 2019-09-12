566b034404a93aa5d02626787fc2e08eefbe3005
moxspoy <mnurilmanbaehaqi@gmail.com>
2019-09-12 12:57:56 +0700

    move folder to xammp from phpstorms project, add some env setting

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by   : MNurilmanBaehaqi
+ * Date         : 12 September 2019
+ * Time         : 12:34
+ * Github       : https://github.com/moxspoy
+ */
+
+header('Location: view');
+
+?>
