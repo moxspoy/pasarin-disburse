9fd52db0c33bdaf89f2abd8df44760beab2417e3
moxspoy <mnurilmanbaehaqi@gmail.com>
2019-09-17 11:59:05 +0700

    change base url to nextar

--- api/config/constant.php
+++ api/config/constant.php
@@ -7,8 +7,8 @@
  * Github       : https://github.com/moxspoy
  */
 define('AUTH_KEY', 'HyzioY7LP6ZoO7nTYKbG8O4ISkyWnX1JvAEVAhtWKZumooCzqp41');
-define('BASE_URL', 'http://localhost/nextar.flip.id/index.php/disbursement');
-define('SERVER_URL', 'https://nextar.flip.id');
+//define('BASE_URL', 'http://localhost/nextar.flip.id/index.php/disbursement');
+define('BASE_URL', 'https://nextar.flip.id');
 define('CLIENT_URL', 'http://localhost/pasarin-disburse');
 
 ?>
